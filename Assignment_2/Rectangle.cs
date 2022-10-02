b7fcc93ee703955572885749e73227b09b3e7db2
Rahul Patel <63693793+Rahul5798@users.noreply.github.com>
2022-10-02 13:04:16 -0400

    The completed Rectangle class

--- Assignment_2/Rectangle.cs
+++ Assignment_2/Rectangle.cs
@@ -8,26 +8,46 @@ namespace Assignment_2
 {
     public class Rectangle
     {
-        public int GetCurrentLength { get; set; }
-
-        public int SetNewLength(int length){ return 0; }
-        public int GetCurrentWidth { get; set; }
-        public int SetNewWidth (int width) {return 0; }
-        public int GetPerimeter { get; set; }
-        public int GetArea { get; set; }
-
-        private int length;
-        private int width;
+        private int Length { get; set; }
+        private int Width { get; set; }
         public Rectangle() // Default Constructor
         {
 
-            length = 1;
-            width = 1;
+            Length = 1;
+            Width = 1;
         }
         public  Rectangle(int length, int width) // Non-Default Constructor
         {
-            
+            Length = length;
+            Width = width;
         }
+        public int GetCurrentLength()
+        {
+            return Length;
+        }
+
+        public int SetNewLength(int length) { 
+            Length = length;
+            return Length;
+        }
+        public int GetCurrentWidth(){
+            return Width;
+        }
+        public int SetNewWidth(int width) {
+            Width = width;
+            return Width;
+        }
+        public int GetPerimeter()
+        {
+            int halfPerimeter = Length + Width;
+            return halfPerimeter*2;
+        }
+        public int GetArea()
+        {
+            int area = Length*Width;
+            return area;
+        }
+
     }
    
 
