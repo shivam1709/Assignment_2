1a62e6167ffca17b6fbd1abdf9cd4b26cf8c45fe
shivam1709 <68727150+shivam1709@users.noreply.github.com>
2022-10-02 12:09:22 -0400

    remaining override

--- Assignment_2/Rectangle.cs
+++ Assignment_2/Rectangle.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace Assignment_2
 {
-    internal class Rectangle
+    public class Rectangle
     {
+        public int GetCurrentLength { get; set; }
+
+        public int SetNewLength(int length){ return 0; }
+        public int GetCurrentWidth { get; set; }
+        public int SetNewWidth (int width) {return 0; }
+        public int GetPerimeter { get; set; }
+        public int GetArea { get; set; }
+
+        private int length;
+        private int width;
+        public Rectangle() // Default Constructor
+        {
+
+            length = 1;
+            width = 1;
+        }
+        public  Rectangle(int length, int width) // Non-Default Constructor
+        {
+            
+        }
     }
+   
+
 }
