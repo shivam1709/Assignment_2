cc974ee5868eae4635153807b0fe1b2bbc40e78b
Rahul Patel <63693793+Rahul5798@users.noreply.github.com>
2022-10-02 14:14:31 -0400

    The completed console application

--- Assignment_2/Program.cs
+++ Assignment_2/Program.cs
@@ -10,7 +10,95 @@ namespace Assignment_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            int length = 0;
+            int width = 0;
+            bool exit = false;
+            String selectedOption;
+            do
+            {
+                Console.Write("Enter the length of the Rectangle: ");
+                length = int.Parse(Console.ReadLine());
+                if (length <= 0)
+                {
+                
+                    Console.WriteLine("Incorrect Input!");
+                }
+            } while (length <= 0);
+            do
+            {
+                Console.Write("Enter the width of the Rectangle: ");
+                width = int.Parse(Console.ReadLine());
+                if (width <= 0)
+                {
+
+                    Console.WriteLine("Incorrect Input!");
+                }
+            } while (width <= 0);
+            Rectangle rectangle = new Rectangle(length,width);
+            do
+            {
+                Console.WriteLine("".PadRight(70, '_'));
+                Console.WriteLine("Menu");
+                Console.WriteLine("".PadRight(70, '_'));
+                Console.WriteLine("1. Get Rectangle Length");
+                Console.WriteLine("2. Change Rectangle Length");
+                Console.WriteLine("3. Get Rectangle Width");
+                Console.WriteLine("4. Change Rectangle Width");
+                Console.WriteLine("5. Get Rectangle Perimeter");
+                Console.WriteLine("6. Get Rectangle Area");
+                Console.WriteLine("7. Exit");
+                selectedOption = Console.ReadLine();
+                Console.WriteLine("".PadRight(70, '_'));
+                switch (selectedOption)
+                {
+                    case "1":
+                        Console.Write($"Current length of the Rectangle = {rectangle.GetCurrentLength()}");
+                        break;
+                    case "2":
+                        Console.Write("Enter the new length for the Rectangle : ");
+                        length = int.Parse(Console.ReadLine());
+                        if(length > 0)
+                        {
+                            rectangle.SetNewLength(length);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Length cannot be less than 1");
+                        }
+                        break;
+                    case "3":
+                        Console.Write($"Current width of the Recttangle = {rectangle.GetCurrentWidth()}");
+                        break;
+                    case "4":
+                        Console.Write("Enter the new width for the Rectangle : ");
+                        width = int.Parse(Console.ReadLine());
+                        if (width > 0)
+                        {
+                            rectangle.SetNewWidth(width);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Width cannot be less than 1");
+                        }
+                        break;
+                    case "5":
+                        Console.Write($"Perimeter of the Rectangle = {rectangle.GetArea()}");
+                        break;
+                    case "6":
+                        Console.Write($"Area of the Rectangle = {rectangle.GetPerimeter()}");
+                        break;
+                    case "7":
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Incorrect option is selected");
+                        break;
+                }
+
+                Console.WriteLine();
+            } while (!exit);
+
+
         }
     }
 }
