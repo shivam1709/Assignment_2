3194c770f9da64b2e321e874a1568bffbce5ce6a
shivam1709 <68727150+shivam1709@users.noreply.github.com>
2022-10-04 15:50:24 -0400

    added packages

---
+++ Assignment_2.Tests/RectangleTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Assignment_2;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace Assignment_2.Tests
+{
+    [TestFixture]
+
+    public class RectangleTest
+    {
+
+        [Test]
+        public void validRectangleLength()
+        {
+            Rectangle rectangle = new Rectangle()
+        }
+    } 
+}
